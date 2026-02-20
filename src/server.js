6aec1581dd2ce442cd2e2d17f667d874444c8e06
karthik <kandellikarthik7@gmail.com>
2026-02-20 12:29:38 +0530

    Fix port for production

--- src/server.js
+++ src/server.js
@@ -61,7 +61,10 @@ app.get("/test-db", async (_, res) => {
 });
 
 /* ================= START SERVER ================= */
+/* ================= START SERVER ================= */
 
-app.listen(5000, () => {
-  console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
