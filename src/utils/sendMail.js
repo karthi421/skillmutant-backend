83a91d848e5df2a716be9db4ad0388644f635115
karthik <kandellikarthik7@gmail.com>
2026-03-05 21:55:50 +0530

    delete account added route

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,10 +1,29 @@
-import { Resend } from "resend";
+/*import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = async ({ to, subject, html }) => {
   await resend.emails.send({
     from: "onboarding@resend.dev", // default working sender
+    to,
+    subject,
+    html,
+  });
+};
+*/
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendMail = async ({ to, subject, html }) => {
+  await transporter.sendMail({
+    from: `"SkillMutant" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
